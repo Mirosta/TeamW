7e16faa7a26508776a68fa7d6e9e37ef093e996f
pollawat <me@pollawat.net>
2014-11-17 16:10:52 +0000

    Created Entity

---
+++ payme/model/entity.py
@@ -0,0 +1,30 @@
+class Entity:
+    empCount = 0
+
+    def __init__(self, name):
+        pass
+
+    def getDebts(self):
+        pass
+
+    def getCredits(self):
+        pass
+
+    def getDebtsAmounts(self):
+        pass
+
+    def getCreditsAmount(self):
+        pass
+
+    def getNetAmounts(self):
+        pass
+
+    def getDebtAmount(self):
+        pass
+
+    def getNetCreditAmount(self):
+        pass
+
+    def getNetAmount(self):
+        pass
+
