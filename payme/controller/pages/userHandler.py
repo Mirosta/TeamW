c397d69d59ec9d16dc6bec8a2e7eeb75888409c6
Thomas Williams <t.r.williamst@googlemail.com>
2015-01-02 21:25:13 +0000

    Added some page and verb handlers
    Started implementing login

--- payme/controller/pages/userHandler.py
+++ payme/controller/pages/userHandler.py
@@ -1,16 +1,38 @@
-from payme.controller.contentHandler import ContentHandler, Parameter, VerbHandler
+from payme.controller.contentHandler import ContentHandler, Parameter, VerbHandler, PageHandler
 
-
-class UserHandler(ContentHandler):
-
-    def __init__(self):
-        super(UserHandler, self).__init__(None, {'login': LoginHandler()})
-
-class LoginHandler(VerbHandler):
+class UserHandler(PageHandler):
 
     def __init__(self):
-        super(LoginHandler, self).__init__('login')
-        self.parameter = Parameter(Parameter.Type.NoParameter, False, False)
+        super(UserHandler, self).__init__(None, Parameter(), {'login': UserHandler.LoginHandler()}) #This page has no view, but it does have verbs
 
-    def getHTML(self, controller, parameter):
-        return super(LoginHandler, self).getHTML(controller, parameter)
+    class LoginHandler(VerbHandler):
+
+        def __init__(self):
+            super(UserHandler.LoginHandler, self).__init__('login')
+
+        def getHTML(self, controller, parameter):
+            return super(UserHandler.LoginHandler, self).getHTML(controller, parameter)
+
+    class LogoutHandler(VerbHandler):
+
+        def __init__(self):
+            super(UserHandler.LogoutHandler, self).__init__('logout')
+
+        def getHTML(self, controller, parameter):
+            return super(UserHandler.LogoutHandler, self).getHTML(controller, parameter)
+
+    class ProfileHandler(VerbHandler):
+
+        def __init__(self):
+            super(UserHandler.ProfileHandler, self).__init__('profile')
+
+        def getHTML(self, controller, parameter):
+            return super(UserHandler.ProfileHandler, self).getHTML(controller, parameter)
+
+    class SettingsHandler(VerbHandler):
+
+        def __init__(self):
+            super(UserHandler.SettingsHandler, self).__init__('settings')
+
+        def getHTML(self, controller, parameter):
+            return super(UserHandler.SettingsHandler, self).getHTML(controller, parameter)
