c65a00c8e529f3de30f111eb95e86c691f721d06
Thomas Williams <t.r.williamst@googlemail.com>
2015-01-03 20:15:21 +0000

    Removed unnecessary parameter line

--- payme/controller/pages/paymentsHandler.py
+++ payme/controller/pages/paymentsHandler.py
@@ -7,7 +7,6 @@ class PaymentsHandler(ModelHandler):
 
     def __init__(self):
         super(PaymentsHandler, self).__init__('payments', {}, 'getAllPayments', Payment, [], [])
-        self.parameter = Parameter(Parameter.Type.NoParameter, False, False)
 
     def getHTML(self, controller, parameter):
         return super(PaymentsHandler, self).getHTML(controller, parameter)
