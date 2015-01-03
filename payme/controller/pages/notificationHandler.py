52ac744d5db4f67dbe1c41e87d406a9a77b6bdf6
pollawat <git@pollawat.net>
2015-01-03 23:11:36 +0000

    Fixed debts

--- payme/controller/pages/notificationHandler.py
+++ payme/controller/pages/notificationHandler.py
@@ -1,7 +1,7 @@
 from payme.controller.modelHandler import ModelHandler, RelatedModel, ReadOnlyFunction
 from payme.model.group import Group
 
-class NotificationHandler(ModelHandler):
-
-    def __init__(self):
-        super(NotificationHandler, self).__init__(None, {}, 'getNotification', User, [], [])
+# class NotificationHandler(ModelHandler):
+#
+#     # def __init__(self):
+#     #     super(NotificationHandler, self).__init__(None, {}, 'getNotification', User, [], [])
