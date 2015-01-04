606c1d975b28ee6df33089792343aa12e7ec2aef
Lyubomir Vasilev <ldv1g12@soton.ac.uk>
2015-01-04 22:54:38 +0000

    change in friends

--- assets/js/pages/friends.js
+++ assets/js/pages/friends.js
@@ -13,9 +13,14 @@ function addFriendsToContainer() {
   //                 '</div>';
 
    var template = '<div class="user-container" style="height:40px;">' +
-                    '<div class="pull-left"><img src="{{profilePicture}}" class="img-rounded" width="25"><span style="font-size:16px;"> {{name}}</span> (<span style="color:{{readOnly.numberClass}};font-weight:bold;">{{readOnly.netAmount}}</span>)</div>' +
-                    '<div class="btn-group pull-right pay-button" role="group"><button type="button" class="btn btn-default" data-toggle="modal" data-target="#add-debt-modal"><i class="glyphicon glyphicon-gbp"></i></button><button type="button" class="btn btn-default"><i class="glyphicon glyphicon-trash"></i></button><button type="button" class="btn btn-default"><b>...</b></button> </div>' +
-                  '</div><hr style="margin-bottom:5px;">';
+                    '<div class="pull-left"><img src="{{profilePicture}}" class="img-rounded" width="25">' +
+                    '<span style="font-size:16px;"> {{name}}</span> ' +
+                    '(<span style="color:{{readOnly.numberClass}};font-weight:bold;">{{readOnly.netAmount}}</span>)</div>' +
+                    '<div class="btn-group pull-right pay-button" role="group">' +
+                    '<button type="button" class="btn btn-default" data-toggle="modal" data-target="#add-debt-modal">' +
+                    '<i class="glyphicon glyphicon-gbp"></i></button><button type="button" class="btn btn-default">' +
+                    '<i class="glyphicon glyphicon-trash"></i></button><button type="button" class="btn btn-default"><b>...</b></button> </div>' +
+                    '</div><hr style="margin-bottom:5px;">';
   var friendsListDiv = $("#friends-list-div");
   friendsListDiv.html("");
 
@@ -30,6 +35,14 @@ function addFriendsToContainer() {
       friendsListDiv.append( processTemplate(template, data[i]) );
     }
   });
-  
-}              
+}
+
+function removeFriend() {
+    
+
+
+
+}
+
+
 
