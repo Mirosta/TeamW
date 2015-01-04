0a7fea7c10ce1bc00cb9e8ad02f17343e390238a
Thomas Williams <t.r.williamst@googlemail.com>
2015-01-04 01:35:01 +0000

    Added friends model class

--- assets/js/model.js
+++ assets/js/model.js
@@ -95,6 +95,7 @@ function ModelClass(modelUrl)
 
 //Create a ModelClass for groups, you can get group objects that extend Model using the .get or .getAll methods
 var groups = new ModelClass("groups");
+var friends = new ModelClass("friends");
 //Model objects have .update, .create and .remove methods
 /*
 Example: Get all groups
