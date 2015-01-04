0e2a070711691bd61d4db141ed74eab54a89ecf4
Trisha Khallaghi <tk7g11@soton.ac.uk>
2015-01-04 15:35:59 +0000

    minor changes, added logout button

--- assets/js/pages/profile.js
+++ assets/js/pages/profile.js
@@ -3,7 +3,7 @@ initialisePage();
 function initialisePage() {
     user.getAll(function(success, data) {
         // $('#user-info-div').html("");
-        var template = '<div class="col-md-8" id="user-info-div" style="font-size:16px;"><label class="read-only-form-label">given name:</label>{{name}}<br><label class="read-only-form-label">family name:</label>{{familyName}}<br><label class="read-only-form-label">email:</label>{{email}}<br></div>';
+        var template = '<div class="col-md-9" id="user-info-div" style="font-size:16px;"><label class="read-only-form-label">given name:</label>{{name}}<br><label class="read-only-form-label">family name:</label>{{familyName}}<br><label class="read-only-form-label">email:</label>{{email}}<br></div>';
 
         var imgTemplate = '<img src="{{profilePicture}}" class="img-circle img-profile">';
 
