f51ccc9e1db2bede0944560618a4db2e372bb9f2
Lyubomir Vasilev <ldv1g12@soton.ac.uk>
2015-01-04 14:19:56 +0000

    grh4gr

--- payme/controller/modelHandler.py
+++ payme/controller/modelHandler.py
@@ -162,7 +162,7 @@ class ModelAddHandler(VerbHandler):
         try:
             entity = validator.validate(postData, self.type)
             # add new entity to database
-            entity.key.put()
+            entity.put()
         except InvalidParameterError:
             return '{"success": 0}'
         return '{"success": 1}'
