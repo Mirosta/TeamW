7f27fc1de3b0e69c4e8446d6f5da101c279b934c
James <jcebuck@gmail.com>
2015-01-04 14:18:40 +0000

    Testing removal from api

--- payme/controller/validator.py
+++ payme/controller/validator.py
@@ -1,10 +1,10 @@
 from payme.controller.exceptions import InvalidParameterError, MissingFieldError
 import json
 
-all_required = ['key']
+all_required = []
 
 
-def validate(json_str, type_class, required_fields=[]):
+def create(json_str, type_class, required_fields=[]):
     required_fields.extend(all_required)
     try:
         json_obj = json.loads(json_str)
@@ -12,5 +12,17 @@ def validate(json_str, type_class, required_fields=[]):
             if key not in json_obj:
                 raise MissingFieldError()
         return type_class(**json_obj)
+    except:
+        raise InvalidParameterError()
+
+
+def retrieve(json_str, type_class, required_fields=[]):
+    required_fields.extend(all_required)
+    try:
+        json_obj = json.loads(json_str)
+        for key in required_fields:
+            if key not in json_obj:
+                raise MissingFieldError()
+        return type_class.query(**json_obj)
     except:
         raise InvalidParameterError()
