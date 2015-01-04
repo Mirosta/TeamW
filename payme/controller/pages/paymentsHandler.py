e89b1d052695df30ab8423953c024884d553742e
Trisha Khallaghi <tk7g11@soton.ac.uk>
2015-01-04 05:20:20 +0000

    added part of getPayee()

--- payme/controller/pages/paymentsHandler.py
+++ payme/controller/pages/paymentsHandler.py
@@ -7,7 +7,7 @@ from payme.model.payment import Payment
 class PaymentsHandler(ModelHandler):
 
     def __init__(self):
-        super(PaymentsHandler, self).__init__('payments', {'add' : PaymentsHandler.AddHandler()}, 'getAllPayments', Payment, [], [])
+        super(PaymentsHandler, self).__init__('payments', {'add' : PaymentsHandler.AddHandler()}, 'getAllPayments', Payment, [], [ReadOnlyFunction("getPayee", "payee")])
         self.parameter = Parameter(Parameter.Type.NoParameter, False, False)
 
     class AddHandler(VerbHandler):
