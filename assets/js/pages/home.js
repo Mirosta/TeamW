a261c36a00167d4cd8447e935e48970c65b08435
Trisha Khallaghi <tk7g11@soton.ac.uk>
2015-01-04 06:39:34 +0000

    dashboard fetching data FO REALS

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -2,30 +2,78 @@
 initialisePage();
 
 function initialisePage() {
-  loadGroupsIntoDashboard();
-  loadFriendsIntoDashboard();
+  $(document).ready(function() {
+    $div = $("#dshbrd-recent-div"); // temporary
+    loadRecentTransactions($div);
+    loadGroupsIntoDashboard();
+    loadFriendsIntoDashboard();
+  });
 }
 
+function loadRecentTransactions($div) {
+  payments.getAll(function(success, paymentData) {
+    initialiseDataTables(paymentData, $div);
+  }, 10, null, "-date");
+}
+
+
+function initialiseDataTables(paymentData, $div) {
+  $(document).ready(function() {
+    // (temp) Column headings for DataTables table
+    var columnHeaders = ["payment by...", "payment to...", "date", "amount paid (£)", "remaining debt"];
+    var data = serverDataToArray(paymentData);
+
+    // Add table to DOM
+    $($div).html("");
+    $($div).append('<table id="recent-transactions-tbl" class="table table-striped" cellspacing="0" width="100%"></table>');
+    
+    // Initialise datatable with options
+    $('#recent-transactions-tbl').dataTable({
+      "bLengthChange" : false,
+      "bFilter" : false, 
+      "paging" : false,
+      "info" : false,
+      "data" : data,
+      "columns" : [
+        { "title" : columnHeaders[0] },
+        { "title" : columnHeaders[1] },
+        { "title" : columnHeaders[2] },
+        { "title" : columnHeaders[3] }
+      ]
+    });
+  });
+}
+
+// Transforms object data from server to array for DataTables
+function serverDataToArray(data) {
+  var finalArray = [];
+  for (i=0; i < data.length; i++) {
+    finalArray.push([data[i].payer, data[i].readOnly.payee, data[i].date, data[i].amount ]);
+  }
+  return finalArray;
+}
+
+
+
 function loadGroupsIntoDashboard() {
-  groups.getAll(function(success, data) {
-    var groupData = data[0];
-    console.log(groupData);
+  var template = '<div>{{name}}<span class="pull-right bold" style="color:{{readOnly.numberClass}};">{{readOnly.netAmount}}</span><br></div>';
+
+  groups.getAll(function(success, groupData) {
     $('#groups-content-div').html("");
-    $('#groups-content-div').append(
-      '<div>' + groupData.name +
-      ' <span class="pull-right bold" style="color:#26A65B;">' + groupData.readOnly.netAmount + '</span><br>' +
-      '</div>');
+    for (i=0; i < groupData.length; i++) {
+      groupData[i].readOnly.numberClass = (groupData[i].readOnly.netAmount >= 0 ? "#26A65B" : "#CF000F");
+      $('#groups-content-div').append( processTemplate(template, groupData[i]) );
+    } 
   });
 }
 
 function loadFriendsIntoDashboard() {
-  friends.getAll(function(success, data) {
-    var friendData = data[0];
-    console.log(friendData);
+  var template = '<div>{{googleID}}<span class="pull-right bold" style="color:{{readOnly.numberClass}};">{{readOnly.netAmount}}</span><br></div>';
+  friends.getAll(function(success, friendData) {
     $('#friends-content-div').html(""); 
-    $('#friends-content-div').append(
-      '<div>' + friendData.googleID +
-      ' <span class="pull-right bold" style="color:#26A65B;">' + friendData.readOnly.netAmount + '</span><br>' +
-      '</div>');
+    for (i=0; i < friendData.length; i++) {
+      friendData[i].readOnly.numberClass = (friendData[i].readOnly.netAmount >= 0 ? "#26A65B" : "#CF000F");
+      $('#friends-content-div').append( processTemplate(template, friendData[i]) );
+    }
   });
 }
